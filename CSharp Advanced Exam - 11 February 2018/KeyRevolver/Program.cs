5bca64deec7f36341f32bdb1d87abe7c42c38768
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-13 09:31:49 +0300

    InProgress

---
+++ CSharp Advanced Exam - 11 February 2018/KeyRevolver/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceOfBullet = int.Parse(Console.ReadLine());
+            int sizeOfBarrel = int.Parse(Console.ReadLine());
+
+            int[] inputBullets = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> bullets = new Stack<int>(inputBullets);
+
+            int[] inputLocks = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> locks = new Queue<int>(inputLocks);
+
+            int intelligence = int.Parse(Console.ReadLine());
+            int countBullets = 0;
+            while (bullets.Count != 0 && locks.Count != 0)
+            {
+                //If the bullet has a smaller or equal size to the current lock, print “Bang!”, then remove the lock. 
+                //If not, print “Ping!” , leaving the lock intact. The bullet is removed in both cases.
+                int currentBullet = bullets.Peek();
+                int currentLock = locks.Peek();
+                if(currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                bullets.Pop();
+                countBullets++;
+                if(countBullets > 0 && countBullets % sizeOfBarrel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+            Console.WriteLine($"{(locks.Count <= 0 ? $"{bullets.Count} bullets left. Earned ${intelligence - (countBullets * priceOfBullet)}" : $"Couldn't get through. Locks left: {locks.Count}")}");
+        }
+    }
+}
---
+++ CSharp Advanced Exam - 11 February 2018/Sneaking/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+namespace Sneaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            char[][] matrix = new char[sizeOfMatrix][];
+
+            FillMatrix(matrix);
+
+            string directions = Console.ReadLine();
+            char[] direction = directions.ToCharArray();
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                char currentDirection = direction[i];
+
+                if(currentDirection == 'U')
+                {
+
+                }
+                else if(currentDirection == 'D')
+                {
+
+                }
+                else if(currentDirection == 'L')
+                {
+
+                }
+                else if(currentDirection == 'R')
+                {
+
+                }
+                else if (currentDirection == 'W')
+                {
+
+                }
+            }
+        }
+
+        public static void FillMatrix(char[][] matrix)
+        {
+            for (int rows = 0; rows < matrix.Length; rows++)
+            {
+                string currentLine = Console.ReadLine();
+                char[] characters = currentLine.ToCharArray();
+                matrix[rows] = characters;
+            }
+        }
+
+        public static void EnemiesMove(char[][] matrix)
+        {
+            for (int rows = 0; rows < matrix.Length; rows++)
+            {
+                for (int cols = 0; cols < matrix[rows].Length; cols++)
+                {
+                    if(matrix[rows][cols] == 'b')
+                    {
+                        matrix[rows][cols] = '.';
+                        if(matrix[rows][cols + 1] == 'S' )
+                        {
+                            matrix[rows][cols + 1] = 'b';
+                        }
+                    }
+                    if(matrix[rows][cols] == 'd')
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public static bool CheckIndex(char[][] matrix, int row, int col)
+        {
+            if(row >= 0 && row < matrix.Length)
+            {
+                if(col >=0 && col < matrix[row].Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
