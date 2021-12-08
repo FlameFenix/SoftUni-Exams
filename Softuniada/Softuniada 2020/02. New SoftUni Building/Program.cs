696ac8cfa687164496cabfe155e35f34cdb729f4
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-12-08 21:49:41 +0200

    Exercises

--- Softuniada/Softuniada 2020/02. New SoftUni Building/Program.cs
+++ Softuniada/Softuniada 2020/02. New SoftUni Building/Program.cs
@@ -6,7 +6,52 @@ namespace _02._New_SoftUni_Building
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n + n / 2, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int counter = 0;
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    if(row == 0 && col == 0)
+                    {
+                        matrix[row, col] = '#';
+                        continue;
+                    }
+
+                    if (row != 0 && 
+                        col != matrix.GetLength(1) - 1 &&
+                        matrix[row - 1, col + 1] == '#')
+                    {
+                        matrix[row, col] = '#';
+                        counter = 0;
+                        continue;
+                    }
+
+                    if (counter == 3)
+                    {
+                        matrix[row, col] = '#';
+                        counter = 0;
+                        continue;
+                    }
+
+                    matrix[row, col] = '.';
+                    counter++;
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
